770afe563e2c3742a8da2dd15479dfa774f6d0a9
siebepattyn-cmd <siebepattyn@gmail.com>
2026-09-19 13:03:54 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'fitsphere-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
